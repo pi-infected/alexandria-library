0ce5243fc97e00701d18d207e961e5f3d2f92489
Paul Irolla <pirolla40@gmail.com>
2022-04-05 21:23:41 +0200

    fix

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -1,3 +1,6 @@
 sudo apt install python3 python3-pip python-is-python3 ruby ruby-dev
 sudo pip uninstall django
-sudo pip install 'django==3.2.12' wordfreq advertools numpy transformers stanza sklearn sentence_transformers nltk phonenumbers email-validator
+sudo pip install 'django==3.2.12' wordfreq advertools numpy transformers stanza sklearn sentence_transformers nltk phonenumbers email-validator python-magic lingua-language-detector
+sudo pip install rdflib -U
+wget https://opensemanticsearch.org/download/open-semantic-search_22.03.04.deb
+sudo apt install ./open-semantic-search_22.03.04.deb
